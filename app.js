6e09138f06d8b616c380a19427d4c3f9ebe5bbef
QuiqueMS <enriquemedranorabak@hotmail.com>
2024-05-12 00:43:08 -0600

    Se agrega la funcionalidad de combate, determina el ganador por medio de su ataque

--- app.js
+++ app.js
@@ -52,11 +52,26 @@ const obtenerPokeRival = () =>{
 }
 
 const combate = ()=>{
-    //alert("Ya quedo tu!")
+    // alert("Ya quedo tu!")
 
-    //Se parsean los dartos de una cadena a un numero
-    const ataqueRival = parseInt(poke2Ataque.textContent);
+    //Se parsean los dartos de una cadena a un numero. Para saber el ataque de cada uno
     const ataquePropio = parseInt(pokeAtaque.textContent); 
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+
+    //Se pasan los nombre de los pokemones para usarlos como texto
+    const namePropio = namePoke.textContent
+    const nameRival = namePoke2.textContent
+
+    //mediante un condicional se evaluar quien gana de los dos pokemons
+    if (ataqueRival > ataquePropio) {
+        //Si el ataque del pokemon es más alto que el Propio, entonces gana Rival
+        alert(nameRival + " es el ganador!!");
+    } else if (ataquePropio > ataqueRival){
+        //En caso que el ataque de pokemon Propio sea más alto que Rival, Gana Propio
+        alert(namePropio + " es el ganador!!");
+    }else{
+        alert("Esto fue un empate");
+    }
 }
 
 //Esto de los eventos va sin parenresis
